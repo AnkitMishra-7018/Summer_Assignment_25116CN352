8f247cc15f5338648394a806f62791b6ff6bd73c
Ankit Mishra <ankitmishra7018@gmail.com>
2026-06-07 13:32:48 +0530

    DAY-1 assignment is uploaded

---
+++ DAY-1/1.c
@@ -0,0 +1,14 @@
+//sum of N natural numbers
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    printf("enter the number N :- ");
+    scanf("%d", &n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("%d",sum);
+    return 0;
+}
---
+++ DAY-1/4.c
@@ -0,0 +1,21 @@
+// program to Count digits in a number
+#include <stdio.h>
+int main()
+{
+    int n,count=0;
+    printf("enter the number --");
+    scanf("%d",&n);
+    if(n==0)
+    {count=1;
+    }
+    else
+    {
+        while(n!=0)
+        {
+            n=n/10;
+            count++;
+        }
+    }
+    printf("the number of digits in the given number is : %d",count);
+    return 0;
+}
