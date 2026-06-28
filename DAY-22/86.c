517d0c976e03c5a71e3b17e4d49406410b27e6ab
Ankit Mishra <ankitmishra7018@gmail.com>
2026-06-28 15:58:57 +0530

    DAY-22

---
+++ DAY-22/86.c
@@ -0,0 +1,23 @@
+// program to Count words in a sentence
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i, wordCount = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t') {
+            wordCount++;
+        }
+    }
+    
+    if (i > 0 && str[i - 1] != ' ' && str[i - 1] != '\n' && str[i - 1] != '\t') {
+        wordCount++;
+    }
+
+    printf("The number of words in the sentence is: %d\n", wordCount);
+
+    return 0;
+}
